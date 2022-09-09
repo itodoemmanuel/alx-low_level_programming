1890eb03ba80284270c3738e2a36d15ba3e7cae1
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-09 16:14:06 -0700

    My commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,23 +3,15 @@
  *main - prints hexadecimal base 0123456789abcdef, using putchar
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n = '0';
-	int a_to_f = 'a';
+	char a;
 
-	while (n <= '9') /*print 0-9*/
+
+	for (a = 'z'; a >= 'a' ; a--)
 	{
-		putchar(n);
-		n++;
-	}
-	while (a_to_f <= 'f') /*print a-f to finish hexbase*/
-	{
-		putchar(a_to_f);
-		a_to_f++;
+		putchar(a);
 	}
 	putchar('\n');
-
 	return (0);
 }
