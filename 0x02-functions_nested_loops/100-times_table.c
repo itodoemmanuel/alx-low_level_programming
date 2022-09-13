aee39359f6f00c4944738ac752eb915b216371fc
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-13 09:19:44 -0700

    my commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_times_table - times table function
  * @n: integer to return
@@ -16,27 +17,25 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				int prod = j * i;
+
 				if (j == 0)
 				{
 					_putchar('0');
-				}
-				else if (prod <= 9)
+				} else if (prod <= 9)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar('0' + prod);
-				}
-				else if (prod > 99)
+				} else if (prod > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar('0' + (prod / 100));
 					_putchar('0' + ((prod / 10) % 10));
 					_putchar('0' + (prod % 10));
-				}
-				else
+				} else
 				{
 					_putchar(',');
 					_putchar(' ');
