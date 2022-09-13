dbddedfc4fc9644098139a7c04155578a3816970
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-13 04:40:01 -0700

    My commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "alx.h"
+/**
+ * print_alphabet - print lowercase alphabet
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
