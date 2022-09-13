fa497b22f91e183c0c656a83f2286d06deeab912
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-13 09:38:40 -0700

    my commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - print sign of number
  * @n: number to check
@@ -11,12 +12,9 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	}
-	else if (n == 0);
-	{
+		(n == 0);
 		_putchar('0');
 		return (0);
-	}
 	else
 	{
 		_putchar('-');
