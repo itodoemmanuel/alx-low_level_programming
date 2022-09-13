0f6cbc156f925080db94f0f8680bbadafe48618f
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-13 13:22:24 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately.
+ * main - Entry point
+ * 
+ * Description: a program that prints _putchar
+ * 
+ * Return: int should return an int
  */
-int main(char c)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
