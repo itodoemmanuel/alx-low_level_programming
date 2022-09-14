80387956d70cd638635ed5fac7967c98bbdbec47
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-14 00:16:39 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i > 0)
+	{
+		printf("%i is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%i is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%i is negative\n", i);
+	}
+	return (0);
+}
