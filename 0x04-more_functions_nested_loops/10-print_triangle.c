cd4bda69a4da33f4b50eda5e0d3155a064514863
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-15 02:48:03 -0700

    My commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: size of triangle
+ */
+
+void print_triangle(int size)
+{
+	int height;
+	int width;
+	int draw;
+
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (height = 1; height <= size; height++)
+	{
+		for (width = 1; width <= (size - height); width++)
+			_putchar(' ');
+
+		for (draw = 1; draw <= height; draw++)
+			_putchar('#');
+
+
+		_putchar('\n');
+	}
+}
