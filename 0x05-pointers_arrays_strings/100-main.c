fd5b436cb9a77ba4e45d3e39911d7c1d09960226
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-19 12:01:29 -0700

    My commit

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -16,16 +16,15 @@ int main(void)
 	printf("%d\n", n);
 	n = _atoi("          ------++++++-----+++++--98");
 	printf("%d\n", n);
-	n = _atoi("214748364");
+	n = _atoi("2147483647");
 	printf("%d\n", n);
-	n = _atoi("0");
+	n = _atoi("-2147483648");
 	printf("%d\n", n);
 	n = _atoi("Suite 402");
 	printf("%d\n", n);
 	n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
 	printf("%d\n", n);
 	n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-	n = _atoi("-++ Sui  te    402 #cisfun :)");
 	printf("%d\n", n);
 	return (0);
 }
