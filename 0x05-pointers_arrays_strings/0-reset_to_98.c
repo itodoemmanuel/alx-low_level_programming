7f69e7d59fdd7fcbf54eab3969924f7344532b4c
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-19 00:34:03 -0700

    My commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - takes a pointer to an int as a parameter
+ * and updates the value it points to to 98
+ *  @n: pointer to int n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
