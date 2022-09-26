2e435c10f090c577adf142242e34619f65f35ca7
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-26 14:09:18 -0700

    My commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 {
 	unsigned int i;
 
-	i = 0;
-	while (i < n)
+	for (i = 0; n < n; i++)
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
