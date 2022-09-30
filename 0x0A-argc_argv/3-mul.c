7eddf63bee6d2b25157fcdf3e35828f34219ebdb
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-30 02:42:06 -0700

    My commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print product of argument numbers
+ * @argc: argument counter
+ * @argv: numbers to multiply
+ * Return: 0 on success, 1 if two arguments not given
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a *b);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
