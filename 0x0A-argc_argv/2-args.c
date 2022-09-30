686b6ce193e9bcde8d44653e7104bc7454da4dd2
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-09-30 07:34:11 -0700

    My commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,23 +1,25 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints the multiplication of two integers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 if true, 1 if false
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int a, b;
-	if (argc == 3)
-	{
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	printf("%d\n", a *b);
-	return (0);
-	}
-	printf("Error\n");
-	return (1);
+int count = 0;
+
+if (argc > 0)
+{
+while (count < argc)
+{
+printf("%s\n", argv[count]);
+count++;
+}
+}
+return (0);
 }
