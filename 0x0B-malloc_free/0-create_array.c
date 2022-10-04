9f009fc10272e076c5f752212b226acc622cd34a
Itodo Emmanuel <itodoemmanuelitodo@icloud.com>
2022-10-04 01:33:22 -0700

    Mycommit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,14 +17,16 @@ if (size == 0)
 return (NULL);
 }
 
+/*Define values with malloc*/
 buffer = (char *) malloc(size * sizeof(c));
-{
+
 if (buffer == 0)
 {
 return (NULL);
 }
 
 else
+{
 position = 0;
 while (position < size)
 {
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
  * _strlen - count array
@@ -8,27 +6,63 @@
  * Return: 1
  */
 
+int _strlen(char *s)
+{
+unsigned int i;
+
+i = 0;
+while (s[i] != '\0') /*count character of string*/
+{
+i++;
+}
+
+return (i);
+}
+
+/**
+ * _strcpy - copy arrays
+ * @src: array of elements
+ * @dest: dest array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+}
+
+/**
+ * _strdup - array for prints a string
+ * @str: array of elements
+ * Return: pointer
+ */
+
 char *_strdup(char *str)
 {
-	char *duplicate_str;
-	int i = 0, len = 0;
+char *dst;
+unsigned int size;
 
-	if (str == NULL) /* validate str input */
-		return (NULL);
-	while (*(str + i))
-		len++, i++;
-	len++; /* add null terminator to length */
+if (str == 0)
+{
+return (NULL);
+}
 
-	uplicate_str = malloc(sizeof(char) * len); /* allocate memory */
+size = _strlen(str) + 1;
 
-	if (duplicate_str == NULL) /* validate memory */
-		return (NULL);
+dst = (char *) malloc(size *sizeof(char));
 
-	i = 0;
-	while (i < len)
-	{
-		*(duplicate_str + i) = *(str + i);
-		i++;
-	}
-	return (duplicate_str);
+if (dst == 0)
+{
+return (NULL);
+}
+_strcpy(dst, str);
+return (dst);
 }
