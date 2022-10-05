148beb3fe675d7ff6b945ee487705e3e2658130e
Itodo Emmanuel Itodo <54606115+itodoemmanuel@users.noreply.github.com>
2022-10-05 00:53:21 -0700

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -34,7 +34,6 @@ int count_word(char *s)
  * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
-char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
